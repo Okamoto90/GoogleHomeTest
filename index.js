6c0c4b293c053232a7505b1cf14847f264063802
HirokiOkamoto <okamoto-hiroki@jp.fujitsu.com>
2017-06-12 14:53:56 +0900

    [add] wiki追加

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const $ = require('jQuery');
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
